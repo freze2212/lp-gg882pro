aea354d88e023091fe948554ed54fc21cd211196
freze2212 <frezeit@kjc.com>
2026-09-05 14:34:12 +0700

    Update link & telegram for g8lt.net

--- js/dev.js
+++ js/dev.js
@@ -1,6 +1,5 @@
 (function () {
-  function getTargetUrl() {
-    return window.REDIRECT_URL || "https://gg8843.com/home/register?id=377696286";
+  function getTargetUrl() { return window.REDIRECT_URL || "https://gg8843.com/home/register?id=377696286";
   }
 
   function goTarget() {
